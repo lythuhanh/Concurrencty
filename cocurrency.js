6a1c87082700a552c836579cd0be113628b25590
lythuhanh <89439872+lythuhanh@users.noreply.github.com>
2021-11-18 20:32:35 +0700

    Update cocurrency.js

--- cocurrency.js
+++ cocurrency.js
@@ -1,15 +1,5 @@
 import { Selector, t } from "testcafe";
 
-const URL = 'https://app-platonl-base-chn.azurewebsites.net/'
-const accountNameBox = Selector('#accountName')
-const passwordBox = Selector('#password')
-const loginBtn = Selector('#btn-submit')
-const userAccountMenuBar = Selector('a').withExactText('Benutzerkonten')
-const editIcon = Selector('i[class="fas fa-pencil-alt"]')
-const activeCheckBox = Selector('#active')
-const saveBtn = Selector('#btn-saveAndClose')
-const conflictForm = Selector('div[class="modal-body dxbs-modal-body"]')
-
 fixture`Concurrency`
 
 test('Open 2 browser', async t => {
@@ -22,8 +12,8 @@ test('Open 2 browser', async t => {
     await t.maximizeWindow()
   //  await t.navigateTo(URL)
     await t
-        .typeText(accountNameBox, 'sys-admin')
-        .typeText(passwordBox,'12345678')
+        .typeText(accountNameBox, '  ')
+        .typeText(passwordBox,'  ')
         .click(loginBtn)
         .click(userAccountMenuBar)
         .click(editIcon)
@@ -33,8 +23,8 @@ test('Open 2 browser', async t => {
     await t.switchToWindow(window2); 
     await t.maximizeWindow()
     await t.wait(3000)
-            .typeText(accountNameBox,'sys-hanh')
-            .typeText(passwordBox,'Abc@a123')
+            .typeText(accountNameBox,'  ')
+            .typeText(passwordBox,'   ')
             .click(loginBtn)
             .click(userAccountMenuBar)
             .click(editIcon)
@@ -49,6 +39,6 @@ test('Open 2 browser', async t => {
     await t
         .click(activeCheckBox)
         .click(saveBtn)
-        .expect(conflictForm.innerText).contains('Widersprüchliche Informationen')
+        .expect(conflictForm.innerText).contains('  ')
         .wait(5000)
 })
